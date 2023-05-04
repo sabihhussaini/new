b2ec90e5d18a859ce355eb218f70c930bc235e28
sabihhussaini <114353845+sabihhussaini@users.noreply.github.com>
2023-05-05 00:36:52 +0530

    Update mdd.py

--- mdd.py
+++ mdd.py
@@ -6,11 +6,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/syeds/OneDrive/Desktop/Early stage disease diagnosis using machine learning/saved models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/syeds/OneDrive/Desktop/Early stage disease diagnosis using machine learning/saved models/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/syeds/OneDrive/Desktop/Early stage disease diagnosis using machine learning/saved models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 
 # sidebar for navigation
